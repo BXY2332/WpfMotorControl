9cd3dbf3370f5388b522a76d1609ed017faecc3f
BXY2332 <BXY980414@OUTLOOK.COM>
2025-12-01 14:35:53 +0800

    Add files via upload

---
+++ MotorBuutonControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public class MotorBuutonControl :ToggleButton
+    {
+        static MotorBuutonControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MotorBuutonControl), new FrameworkPropertyMetadata(typeof(MotorBuutonControl)));
+        }
+
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Geometry), typeof(MotorBuutonControl), new PropertyMetadata(null));
+
+        public bool IsRunning
+        {
+            get => (bool)GetValue(IsRunningProperty);
+            set => SetValue(IsRunningProperty, value);
+        }
+        public static readonly DependencyProperty IsRunningProperty =
+            DependencyProperty.Register("IsRunning", typeof(bool), typeof(MotorBuutonControl), new PropertyMetadata(false));
+    }
+}
